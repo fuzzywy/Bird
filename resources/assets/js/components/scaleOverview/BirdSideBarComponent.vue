68d902235da3269b108ef4e5bc9390deebb613ba
lijian <591466539@qq.com>
2018-09-13 16:16:01 +0800

    1、sideBar折叠功能
    Signed-off-by: lijian <591466539@qq.com>

--- resources/assets/js/components/scaleOverview/BirdSideBarComponent.vue
+++ resources/assets/js/components/scaleOverview/BirdSideBarComponent.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<b-card nobody header="<b>网络概览</b>">
+		<b-card header="<b>网络概览</b>" @click='switches' :class='state'>
 			<b-list-group flush>
 				<span v-show="birdSideBarLoadStatus == 1">Loading</span>
 				<b-list-group-item button v-show="birdSideBarLoadStatus == 2"
@@ -9,12 +9,33 @@
 					:index='item.id'
 					:name='item.routertag'
 					@click='leftClick'
-				>{{ item.Content }}</b-list-group-item>
+				>{{ item.Content }}
+				</b-list-group-item>
 				<span v-show="birdSideBarLoadStatus == 3">SideBar loaded unsuccessfully!</span>
 			</b-list-group>
 		</b-card>
 	</div>
 </template>
+
+<style>
+	/*.card-body > .list-group {
+		transition:height 2s;
+		-moz-transition:height 2s;*/ /* Firefox 4 */
+		/*-webkit-transition:height 2s;*/ /* Safari and Chrome */
+		/*-o-transition:height 2s;*/ /* Opera */
+	/*}*/
+
+	.card-body-disappear > .card-body {
+		/*height: 0px;*/
+		display: none;
+	}
+
+	.card-body-appear > .card-body {
+		display: block;
+		/*height: 200px;*/
+		
+	}
+</style>
 
 <script>
 	export default {
@@ -23,7 +44,8 @@
 				posts: [
 					/*{ id: 1, Content: '指标概览', routertag: 'indexoverview' },
 					{ id: 2, Content: '规模概览', routertag: 'scaleoverview' }*/
-				]
+				],
+				state: 'card-body-appear'
 			}
 		},
 		created() {
@@ -44,6 +66,14 @@
 			leftClick: function (event) {
 				// this.bus.$emit('leftClick',event.target.name)
 				this.bus.$emit('leftClickData', event.target.name)
+				this.state = 'card-body-disappear'
+			},
+			switches: function() {
+				if ( this.state == 'card-body-disappear' ) {
+					this.state = 'card-body-appear'
+				} else {
+					this.state = 'card-body-disappear'
+				}
 			}
 		}
 	}
