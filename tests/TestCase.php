cb6a7949c494f1fb2177a268c4470a3e32126b96
fuzzywy <fuzzywy@163.com>
2018-11-13 16:10:02 +0800

    Test gerrit phase3
    Change-Id: I04be259e93611f6d23fd7b61ecdf1ff2322c3b4d

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,6 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    // Test gerrit phase 2	
+    // Test gerrit phase 3	
     use CreatesApplication;
 }
