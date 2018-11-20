bf7473e7b3e0e8d392cf6ded061427a2aa662fe0
fuzzywy <fuzzywy@163.com>
2018-11-20 18:12:47 +0800

    Github webhook phase 2
    Change-Id: I3044412a1d96267642b60292c56a6ef440341eae

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
 	// Test gerrit phase 3
-	// Test gerrit github-webhook	
+	// Test gerrit github-webhook
+	// Test gerrit github-webhook3	
     use CreatesApplication;
 }
