b001caf166850f86f3bc124c6e7b96c4ef801ee5
fuzzywy <fuzzywy@163.com>
2018-11-21 16:55:23 +0800

    change LF format
    Change-Id: I8f636bb0c4dba8ba168a11617e01d1b9e8d4b953

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-git clone https://eDeploy:ehub1234@github.com/fuzzywy/Docker-Bird.git
-cd Docker-Bird/
-chmod +x build/build-product.sh
-./build/build-product.sh
 
+git clone https://eDeploy:ehub1234@github.com/fuzzywy/Docker-Bird.git
+
+cd Docker-Bird
+
+chmod +x build/build-product.sh
+
+build/build-product.sh
