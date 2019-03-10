17c81ba94a2232ab4865c6830cd7742b188570a0
xujin <ferrytsui@163.com>
2019-03-11 00:43:11 +0800

    2019/03/11 Signed-off-by: xujin <ferrytsui@163.com>
    Change-Id: Iae70288389351756b678dfd8bd8fdcaefc9db847

---
+++ database/factories/B_VOLTE_FDD_DAYFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\VOLTE_FDD_DAY::class, function (Faker $faker) {
+    return [
+        'day_id' => $faker->date('Y/m/d'),
+        'location' => $faker->city,
+        'access' => number_format($faker->randomFloat(2, 98, 100), 2),
+        'lost' => number_format($faker->randomFloat(2, 0, 0.3), 2),
+        'handover' => number_format($faker->randomFloat(2, 98, 100), 2),
+        'srvcc' => number_format($faker->randomFloat(2, 50, 100), 2),
+        'upackagelost' => number_format($faker->randomFloat(2, 0, 1), 2),
+        'dpackagelost' => number_format($faker->randomFloat(2, 0, 1), 2),
+    ];
+});
