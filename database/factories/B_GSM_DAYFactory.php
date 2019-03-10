be8ac60821f54ac2a6e4774728cae2450a285df9
xujin <ferrytsui@163.com>
2019-03-11 00:43:11 +0800

    湖北移动Bird库，编写了B_开头的没数据的表的factory代码
    Change-Id: Iae70288389351756b678dfd8bd8fdcaefc9db847

---
+++ database/factories/B_GSM_DAYFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\GSM_DAY::class, function (Faker $faker) {
+    return [
+        'day_id' => $faker->date('Y/m/d'),
+        'location' => $faker->city,
+        'access' => number_format($faker->randomFloat(2, 98, 100), 2),
+        'lost' => number_format($faker->randomFloat(2, 0, 0.3), 2),
+        'handover' => number_format($faker->randomFloat(2, 98, 100), 2),
+    ];
+});
