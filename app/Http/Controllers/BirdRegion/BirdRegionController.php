9f77dead900cf622ab445fbe4ea48002447b0364
lijian1234 <www.591466539@qq.com>
2019-03-14 14:16:55 +0800

    MENU导航调整
    Change-Id: Id9f31f8f57c7568b92b605f0b5d06b7bed2e0c01
    Signed-off-by: lijian1234 <www.591466539@qq.com>

--- app/Http/Controllers/BirdRegion/BirdRegionController.php
+++ app/Http/Controllers/BirdRegion/BirdRegionController.php
@@ -19,6 +19,7 @@ class BirdRegionController extends Controller
       array("id" => 1, "label" => "江苏省", "value" => "jiangsu", "cities" => array( array("id" => 0, "label" => "南京", "value" => "nanjing" ), array("id" => 0, "label" => "常州", "value" => "changzhou" ) ) ),
       array("id" => 2, "label" => "湖北省", "value" => "hubei", "cities" => array( array("id" => 0, "label" => "武汉", "value" => "wuhan" ), array("id" => 0, "label" => "荆州", "value" => "jingzhou" ) ) ),
       array("id" => 3, "label" => "广东省", "value" => "guangdong", "cities" => array( array("id" => 0, "label" => "广州", "value" => "guangzhou" ), array("id" => 0, "label" => "清远", "value" => "qingyuan" ) ) )
+
     );
     return $arr;
   }
